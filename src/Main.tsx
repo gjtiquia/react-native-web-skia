f14a96b6f52215073bf25650e06bc6b0370e4683
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-06 19:50:15 +0800

    chore: setup skia on mobile
    The canva element does not play well with NativeWind and should use old-school style
    Setting it to flex:1, which is the default, it will follow the size of an outer View component
    
    Can also get the layout size from the onLayout event
    https://shopify.github.io/react-native-skia/docs/canvas/overview/
    https://reactnative.dev/docs/view#onlayout
    https://reactnative.dev/docs/layoutevent

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,15 @@
+import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { HelloWorld } from "src/components"
 
 const Main = () => {
     return (
         <SafeAreaProvider>
-            <HelloWorld />
+            <View className="flex-1 justify-center">
+                <View className="h-4/6">
+                    <HelloWorld />
+                </View>
+            </View>
         </SafeAreaProvider>
     );
 }
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,10 +1,20 @@
-import { Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
 
 export const HelloWorld = () => {
+    const size = 256;
+    const r = size * 0.33;
     return (
-        <SafeAreaView className="flex-1 items-center justify-center">
-            <Text className="font-bold text-3xl">Hello World</Text>
-        </SafeAreaView>
-    )
-}
+        <Canvas style={{ flex: 1, backgroundColor: "red" }} onLayout={e => console.log(e.nativeEvent.layout)}>
+            <Group blendMode="multiply">
+                <Circle cx={r} cy={r} r={r} color="cyan" />
+                <Circle cx={size - r} cy={r} r={r} color="magenta" />
+                <Circle
+                    cx={size / 2}
+                    cy={size - r}
+                    r={r}
+                    color="yellow"
+                />
+            </Group>
+        </Canvas>
+    );
+};
